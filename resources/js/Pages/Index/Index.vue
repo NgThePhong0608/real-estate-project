a1257e69075be8d81e0141849d48d1e48a9cd42b
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-02-27 03:12:01 +0700

    create first spa application

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Index
+    </div>
+    <Link href="/show">Show page</Link>
+</template>
+
+<script setup>
+import { Link} from "@inertiajs/inertia-vue3";
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
 
 createInertiaApp({
-    resolve: async  (name) => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    resolve: async (name) => {
+        const pages = import.meta.glob('./Pages/**/*.vue')
+
         return (await pages[`./Pages/${name}.vue`]())
     },
     setup({ el, App, props, plugin }) {
