7e56bffe6d00b47e1fef8ac02359cbf7a118b096
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-02-27 03:20:34 +0700

    passing data to vue component

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -3,9 +3,13 @@
         Index
     </div>
     <Link href="/show">Show page</Link>
+    <div> The message is {{ message }} </div>
 </template>
 
 <script setup>
 import { Link} from "@inertiajs/inertia-vue3";
+defineProps({
+    message: String
+})
 
 </script>
