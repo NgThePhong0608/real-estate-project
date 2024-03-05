d94a38546f4205d5a70e1cec58b019ad7886e375
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-05 14:05:35 +0700

    Refactor form input styling and error handling

--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -3,50 +3,66 @@
         <div class="grid grid-cols-6 gap-4">
             <div class="col-span-2">
                 <label class="label">Beds</label>
-                <input class="input" v-model.number="form.beds" type="text" />
-                <div v-if="form.errors.beds"> {{ form.errors.beds }}</div>
+                <input v-model.number="form.beds" type="text" class="input" />
+                <div v-if="form.errors.beds" class="input-error">
+                    {{ form.errors.beds }}
+                </div>
             </div>
 
             <div class="col-span-2">
                 <label class="label">Baths</label>
-                <input class="input" v-model.number="form.baths" type="text" />
-                <div v-if="form.errors.baths"> {{ form.errors.baths }}</div>
+                <input v-model.number="form.baths" type="text" class="input" />
+                <div v-if="form.errors.baths" class="input-error">
+                    {{ form.errors.baths }}
+                </div>
             </div>
 
             <div class="col-span-2">
                 <label class="label">Area</label>
-                <input class="input" v-model.number="form.area" type="text" />
-                <div v-if="form.errors.area"> {{ form.errors.area }}</div>
+                <input v-model.number="form.area" type="text" class="input" />
+                <div v-if="form.errors.area" class="input-error">
+                    {{ form.errors.area }}
+                </div>
             </div>
 
             <div class="col-span-4">
                 <label class="label">City</label>
-                <input class="input" v-model="form.city" type="text" />
-                <div v-if="form.errors.city"> {{ form.errors.city }}</div>
+                <input v-model="form.city" type="text" class="input" />
+                <div v-if="form.errors.city" class="input-error">
+                    {{ form.errors.city }}
+                </div>
             </div>
 
             <div class="col-span-2">
                 <label class="label">Post Code</label>
-                <input class="input" v-model="form.code" type="text" />
-                <div v-if="form.errors.code"> {{ form.errors.code }}</div>
+                <input v-model="form.code" type="text" class="input" />
+                <div v-if="form.errors.code" class="input-error">
+                    {{ form.errors.code }}
+                </div>
             </div>
 
             <div class="col-span-4">
                 <label class="label">Street</label>
-                <input class="input" v-model="form.street" type="text" />
-                <div v-if="form.errors.street"> {{ form.errors.street }}</div>
+                <input v-model="form.street" type="text" class="input" />
+                <div v-if="form.errors.street" class="input-error">
+                    {{ form.errors.street }}
+                </div>
             </div>
 
             <div class="col-span-2">
                 <label class="label">Street Nr</label>
-                <input class="input" v-model.number="form.street_num" type="text" />
-                <div v-if="form.errors.street_num"> {{ form.errors.street_num }}</div>
+                <input v-model.number="form.street_num" type="text" class="input" />
+                <div v-if="form.errors.street_num" class="input-error">
+                    {{ form.errors.street_num }}
+                </div>
             </div>
 
             <div class="col-span-6">
                 <label class="label">Price</label>
-                <input class="input" v-model.number="form.price" type="text" />
-                <div v-if="form.errors.price"> {{ form.errors.price }}</div>
+                <input v-model.number="form.price" type="text" class="input" />
+                <div v-if="form.errors.price" class="input-error">
+                    {{ form.errors.price }}
+                </div>
             </div>
 
             <div class="col-span-6">
