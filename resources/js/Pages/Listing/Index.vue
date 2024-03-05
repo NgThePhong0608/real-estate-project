a1c0450468e80af8277872d568604d6d9cdc1300
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-05 13:17:34 +0700

    custom UI and grid responsive design

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,24 +1,27 @@
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3'
 import ListingAddress from "../../Components/ListingAddress.vue";
+import Box from "../../Components/UI/Box.vue";
 defineProps({
     listings: Array
 })
 </script>
 
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <div>
-            <Link :href="`/listing/${listing.id}`">
-                <ListingAddress :listing="listing" />
-            </Link>
-        </div>
-        <div>
-            <Link :href="`/listing/${listing.id}/edit`">Edit</Link>
-        </div>
-        <div>
-            <Link :href="`/listing/${listing.id}`" method="DELETE">Delete</Link>
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <Link :href="`/listing/${listing.id}`">
+                    <ListingAddress :listing="listing" />
+                </Link>
+            </div>
+            <div>
+                <Link :href="`/listing/${listing.id}/edit`">Edit</Link>
+            </div>
+            <div>
+                <Link :href="`/listing/${listing.id}`" method="DELETE">Delete</Link>
+            </div>
+        </Box>
     </div>
 </template>
 
