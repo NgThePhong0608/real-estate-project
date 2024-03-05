431e4b818a553b9f3879c42263807a03025c75ad
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-05 10:52:11 +0700

    display all listings record to vue component

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,9 +1,17 @@
 <script setup>
-
+import { Link } from '@inertiajs/inertia-vue3'
+import ListingAddress from "../../Components/ListingAddress.vue";
+defineProps({
+    listings: Array
+})
 </script>
 
 <template>
-
+    <div v-for="listing in listings" :key="listing.id">
+        <Link :href="`/listing/${listing.id}`">
+            <ListingAddress :listing="listing"></ListingAddress>
+        </Link>
+    </div>
 </template>
 
 <style scoped>
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,9 +1,16 @@
 <script setup>
-
+import { Link } from "@inertiajs/inertia-vue3";
+import ListingAddress from "../../Components/ListingAddress.vue";
+defineProps({
+    listing: Object
+})
 </script>
 
 <template>
-
+    <div>
+        <ListingAddress :listing="listing"></ListingAddress>
+        <Link :href="`/listing`"> Go back </Link>
+    </div>
 </template>
 
 <style scoped>
