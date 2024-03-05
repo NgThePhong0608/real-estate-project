19692a5606f78454295906b48c6cfa279b29bc14
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-05 11:24:12 +0700

    render flash message

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,25 @@
 <script setup>
-import { Link } from "@inertiajs/inertia-vue3";
+import { Link, usePage } from "@inertiajs/inertia-vue3";
+import { computed} from "vue";
+// page.props.value.flash.success
+const page = usePage();
+const flashMessage = computed(
+    () => page.props.value.flash.success
+);
 </script>
 
 <template>
     <Link href="/">Main Page</Link> &nbsp;
     <Link href="/show">Show Page</Link>
+    <div v-if="flashMessage" class="success">
+        {{ flashMessage }}
+    </div>
     <slot>Default</slot>
 </template>
 
 <style scoped>
-
+.success{
+    background-color: green;
+    color: white;
+}
 </style>
