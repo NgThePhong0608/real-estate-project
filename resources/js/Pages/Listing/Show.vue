d6572fe390786df33617b856613561a89db58633
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-05 14:46:52 +0700

    Refactor useMonthlyPayment to include totalPaid and totalInterest

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -13,7 +13,9 @@ const props = defineProps({
 const interestRate = ref(2.5);
 const duration = ref(25);
 
-const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration);
+const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(
+    props.listing.price, interestRate, duration,
+)
 
 
 </script>
@@ -49,6 +51,26 @@ const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, 
                     <div class="text-gray-600 dark:text-gray-300 mt-2">
                         <div class="text-gray-400">Your monthly payment</div>
                         <Price :price="monthlyPayment" class="text-3xl font-bold" />
+                    </div>
+                    <div class="mt-2 text-gray-500">
+                        <div class="flex justify-between">
+                            <div>Total paid</div>
+                            <div>
+                                <Price :price="totalPaid" class="font-medium" />
+                            </div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div>Principal paid</div>
+                            <div>
+                                <Price :price="listing.price" class="font-medium" />
+                            </div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div>Interest paid</div>
+                            <div>
+                                <Price :price="totalInterest" class="font-medium" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Box>
