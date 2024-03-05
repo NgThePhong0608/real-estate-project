5c0e3f4f2156072edfe3b3ff41edbbcb9770a5f5
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-05 14:21:20 +0700

    monthly payment calculation

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -3,8 +3,19 @@ import ListingAddress from "../../Components/ListingAddress.vue";
 import ListingSpace from "../../Components/ListingSpace.vue";
 import Price from "../../Components/Price.vue";
 import Box from '../../Components/UI/Box.vue';
-defineProps({
+import { ref, computed } from "vue";
+
+const props = defineProps({
     listing: Object
+})
+
+const interestRate = ref(2.5);
+const duration = ref(25);
+const monthlyPayment = computed(() => {
+    const principle = props.listing.price
+    const monthlyInterest = interestRate.value / 100 / 12
+    const numberOfPaymentMonths = duration.value * 12
+    return principle * monthlyInterest * (Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) / (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
 })
 </script>
 
@@ -18,16 +29,29 @@ defineProps({
                 <template #header>
                     Basic info
                 </template>
-                <Price :price="listing.price" class="text-2xl font-bold" />
-                <ListingSpace :listing="listing" class="text-lg" />
-                <ListingAddress :listing="listing" class="text-gray-500" />
+                <Price :price="props.listing.price" class="text-2xl font-bold" />
+                <ListingSpace :listing="props.listing" class="text-lg" />
+                <ListingAddress :listing="props.listing" class="text-gray-500" />
             </Box>
 
             <Box>
                 <template #header>
-                    Offer
+                    Monthly Payment
                 </template>
-                Make an offer
+                <div>
+                    <label class="label">Interest rate ({{ interestRate }}%)</label>
+                    <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1"
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+
+                    <label class="label">Duration ({{ duration }} years)</label>
+                    <input v-model.number="duration" type="range" min="3" max="35" step="1"
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">Your monthly payment</div>
+                        <Price :price="monthlyPayment" class="text-3xl font-bold" />
+                    </div>
+                </div>
             </Box>
         </div>
     </div>
