92da4148008e8bc4569c8c35881ff9d75030bbca
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-05 14:41:11 +0700

    Refactor listing display in Index.vue

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,31 +1,14 @@
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3'
-import ListingAddress from "../../Components/ListingAddress.vue";
-import Box from "../../Components/UI/Box.vue";
-import ListingSpace from "../../Components/ListingSpace.vue";
-import Price from "../../Components/Price.vue";
+import Listing from './Index/Components/Listing.vue';
 defineProps({
     listings: Array
 })
+
 </script>
 
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Box v-for="listing in listings" :key="listing.id">
-            <div>
-                <Link :href="`/listing/${listing.id}`">
-                <Price :price="listing.price" class="text-2xl font-bold" />
-                <ListingSpace :listing="listing" class="text-lg" />
-                <ListingAddress :listing="listing" class="text-gray-500" />
-                </Link>
-            </div>
-            <div>
-                <Link :href="`/listing/${listing.id}/edit`">Edit</Link>
-            </div>
-            <div>
-                <Link :href="`/listing/${listing.id}`" method="DELETE">Delete</Link>
-            </div>
-        </Box>
+        <Listing v-for="listing in listings" :key="listing.id" :listing="listing" />
     </div>
 </template>
 
