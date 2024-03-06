03a0f2e32866cf22ce8efc900f6e0de3b0289499
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-06 20:57:56 +0700

    protect route

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,21 @@ use App\Http\Controllers\ListingController;
 */
 
 Route::get('/', [IndexController::class, 'index']);
-Route::get('/show', [IndexController::class, 'show']);
-Route::resource('listing', ListingController::class);
-Route::get('login', [AuthController::class, 'create'])->name('login');
-Route::post('login', [AuthController::class, 'store'])->name('login.store');
-Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
+Route::get('/show', [IndexController::class, 'show'])
+    ->middleware('auth');
+
+Route::resource('listing', ListingController::class)
+    ->only(['create', 'store', 'edit', 'update', 'destroy'])
+    ->middleware('auth');
+
+Route::resource('listing', ListingController::class)
+    ->except(['create', 'store', 'edit', 'update', 'destroy']);
+
+Route::get('login', [AuthController::class, 'create'])
+    ->name('login');
+
+Route::post('login', [AuthController::class, 'store'])
+    ->name('login.store');
+
+Route::delete('logout', [AuthController::class, 'destroy'])
+    ->name('logout');
