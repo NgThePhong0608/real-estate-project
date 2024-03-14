640d7f26d6ed4766cc653a215253de748b3aa670
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-14 17:59:26 +0700

    delete listing images action

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -15,9 +15,19 @@
     </Box>
     <Box v-if="listing.images.length" class="mt-4">
         <template #header>Current Listing Images</template>
-        <section class="mt-4 grid grid-cols-4 gap-4" >
-            <div v-for="image in listing.images" :key="image.id">
-                <img :src="image.src" class="w-full h-32 object-cover rounded-md">
+        <section class="mt-4 grid grid-cols-4 gap-4">
+            <div
+                v-for="image in listing.images" :key="image.id"
+                class="flex flex-col justify-between">
+                <img :src="image.src" class="w-full h-32 object-cover rounded-md" alt="Listing images">
+                <Link
+                    :href="`/realtor/listing/${listing.id}/image/${image.id}`"
+                    method="DELETE"
+                    as="button"
+                    class="mt-2 btn-outline text-xs"
+                >
+                    Delete
+                </Link>
             </div>
         </section>
     </Box>
@@ -29,6 +39,7 @@ import {useForm} from "@inertiajs/inertia-vue3";
 import {computed} from "vue";
 import {Inertia} from "@inertiajs/inertia";
 import NProgess from "nprogress";
+import {Link} from "@inertiajs/inertia-vue3";
 
 const props = defineProps({
     listing: Object
