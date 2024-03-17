191bb4200cec9bb05c044d22ffd54cfa762aa55a
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-17 20:55:38 +0700

    Revert "sending database notification"
    This reverts commit 143f128db8022e8f7716f201ddd1d9b9b8f8a2f9.

--- app/Notifications/OfferMade.php
+++ app/Notifications/OfferMade.php
@@ -2,7 +2,6 @@
 
 namespace App\Notifications;
 
-use App\Models\Offer;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -15,7 +14,7 @@ class OfferMade extends Notification
     /**
      * Create a new notification instance.
      */
-    public function __construct(private Offer $offer)
+    public function __construct()
     {
         //
     }
@@ -27,7 +26,7 @@ class OfferMade extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['database'];
+        return ['mail'];
     }
 
     /**
@@ -36,9 +35,9 @@ class OfferMade extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
     }
 
     /**
@@ -49,10 +48,7 @@ class OfferMade extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            'offer_id' => $this->offer->id,
-            'listing_id' => $this->offer->listing_id,
-            'amount' => $this->offer->amount,
-            'bidder_id' => $this->offer->bidder_id
+            //
         ];
     }
 }
