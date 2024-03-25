532148700004837136e741feb0383f67038b4919
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-25 21:59:51 +0700

    show notification count

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -24,8 +24,8 @@ const notificationCount = computed(
                 </div>
                 <div v-if="user" class="flex items-center gap-4">
                     <div class="text-gray-500 relative pr-2 py-2 text-lg">
-                        <div v-if="notificationCount"
-                            class="absolute right-0 top-0 w-5 h-5 bg-red-700 dark:bg-red-400 text-white font-medium border border-white dark:border-gray-900 rounded-full text-xs text-center">
+                        🔔
+                        <div class="absolute right-0 top-0 w-5 h-5 bg-red-700 dark:bg-red-400 text-white font-medium border border-white dark:border-gray-900 rounded-full text-xs text-center">
                             {{ notificationCount }}
                         </div>
                     </div>
