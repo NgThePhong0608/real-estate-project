92b15e7ca1568032e84846cc393637a289da560e
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-25 21:53:04 +0700

    notification database

--- database/migrations/2024_03_25_144930_create_notifications_table.php
+++ database/migrations/2024_03_25_144930_create_notifications_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('notification', function (Blueprint $table) {
+        Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('notification');
+        Schema::dropIfExists('notifications');
     }
 };
