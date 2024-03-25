58bb3bb379249da1c9ee9f0fb6f0077258914791
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-25 22:05:57 +0700

    empty state

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -6,9 +6,9 @@ import Box from '../../Components/UI/Box.vue';
 import MakeOffer from "../Listing/Show/Components/MakeOffer.vue";
 import OfferMade from "../Listing/Show/Components/OfferMade.vue";
 import EmptyState from "../../Components/UI/EmptyState.vue";
-import { computed, ref } from "vue";
-import { useMonthlyPayment } from "../../Composables/useMonthlyPayment";
-import { usePage } from "@inertiajs/inertia-vue3";
+import {computed, ref} from "vue";
+import {useMonthlyPayment} from "../../Composables/useMonthlyPayment";
+import {usePage} from "@inertiajs/inertia-vue3";
 
 const props = defineProps({
     listing: Object,
@@ -20,7 +20,7 @@ const duration = ref(25);
 
 const offer = ref(props.listing.price)
 
-const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(
+const {monthlyPayment, totalPaid, totalInterest} = useMonthlyPayment(
     offer, interestRate, duration,
 )
 const page = usePage();
@@ -32,20 +32,20 @@ const user = computed(
 
 <template>
     <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
-        <Box class="md:col-span-7 flex items-center">
-            <div v-if="listing.images.length" class="grid grid-cols-2 gap-2">
+        <Box v-if="listing.images.length"  class="md:col-span-7 flex items-center">
+            <div class="grid grid-cols-2 gap-2">
                 <img v-for="image in listing.images" :key="image.id" :src="image.src" alt="Listing images">
             </div>
-            <EmptyState v-else class="md:col-span-7 flex items-center">No images</EmptyState>
         </Box>
+        <EmptyState v-else class="md:col-span-7 flex items-center">No images</EmptyState>
         <div class="md:col-span-5 flex flex-col gap-4">
             <Box>
                 <template #header>
                     Basic info
                 </template>
-                <Price :price="props.listing.price" class="text-2xl font-bold" />
-                <ListingSpace :listing="props.listing" class="text-lg" />
-                <ListingAddress :listing="props.listing" class="text-gray-500" />
+                <Price :price="props.listing.price" class="text-2xl font-bold"/>
+                <ListingSpace :listing="props.listing" class="text-lg"/>
+                <ListingAddress :listing="props.listing" class="text-gray-500"/>
             </Box>
 
             <Box>
@@ -55,42 +55,42 @@ const user = computed(
                 <div>
                     <label class="label">Interest rate ({{ interestRate }}%)</label>
                     <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1"
-                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+                           class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
 
                     <label class="label">Duration ({{ duration }} years)</label>
                     <input v-model.number="duration" type="range" min="3" max="35" step="1"
-                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+                           class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
 
                     <div class="text-gray-600 dark:text-gray-300 mt-2">
                         <div class="text-gray-400">Your monthly payment</div>
-                        <Price :price="monthlyPayment" class="text-3xl font-bold" />
+                        <Price :price="monthlyPayment" class="text-3xl font-bold"/>
                     </div>
                     <div class="mt-2 text-gray-500">
                         <div class="flex justify-between">
                             <div>Total paid</div>
                             <div>
-                                <Price :price="totalPaid" class="font-medium" />
+                                <Price :price="totalPaid" class="font-medium"/>
                             </div>
                         </div>
                         <div class="flex justify-between">
                             <div>Principal paid</div>
                             <div>
-                                <Price :price="listing.price" class="font-medium" />
+                                <Price :price="listing.price" class="font-medium"/>
                             </div>
                         </div>
                         <div class="flex justify-between">
                             <div>Interest paid</div>
                             <div>
-                                <Price :price="totalInterest" class="font-medium" />
+                                <Price :price="totalInterest" class="font-medium"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </Box>
 
-            <MakeOffer v-if="user" :listing-id="listing.id" :price="listing.price" @offer-updated="offer = $event" />
+            <MakeOffer v-if="user" :listing-id="listing.id" :price="listing.price" @offer-updated="offer = $event"/>
 
-            <OfferMade v-if="user && offerMade" :offer="offerMade" />
+            <OfferMade v-if="user && offerMade" :offer="offerMade"/>
         </div>
     </div>
 </template>
