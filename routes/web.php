7357d510f9a64234acfaf653a815e632174adb18
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-03-25 22:30:34 +0700

    render all notification and mark as read notification action

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\RealtorListingImageController;
 use App\Http\Controllers\ListingOfferController;
 use App\Http\Controllers\RealtorListingAcceptOfferController;
 use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\NotificationSeenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,6 +37,10 @@ Route::resource('listing.offer', ListingOfferController::class)
 Route::resource('notification', NotificationController::class)
     ->middleware('auth')
     ->only(['index']);
+
+Route::put('/notification/{notification}/seen', NotificationSeenController::class)
+    ->middleware('auth');
+
 
 Route::get('login', [AuthController::class, 'create'])
     ->name('login');
